436cec3cc140c4b7ae2e5b5ae0557e8c1ea2d995
Cole Faust <colecfaust@gmail.com>
2016-08-15 19:55:21 -0700

    Fixed padding and should be 24 bpp not 8

--- src/EasyImg.cpp
+++ src/EasyImg.cpp
@@ -27,15 +27,14 @@ void writeBMP(string filename, unsigned char** pixels, uint32_t w, uint32_t h) {
   header[1] = 'M';
   uint32_t fileSize = 54+h*(3*w+(3*w)%4);
   writeUint32(header+2, fileSize);
-  uint32_t pixArrayOffset = 54;
-  writeUint32(header+10, pixArrayOffset);
+  writeUint32(header+10, 54);              //pixel array offset
 
   //==========DIB header=========
   writeUint32(header+14, 40); //dib header size
   writeUint32(header+18, w);  //width of image
   writeUint32(header+22, h);  //height of image
   writeUint16(header+26, 1);  //color planes (must be 1?)
-  writeUint16(header+28, 8);  //bpp, using 1 char per pixel means 8
+  writeUint16(header+28, 24);  //bpp, using 1 char per pixel means 8
   writeUint32(header+30, 0);  //compression 0=none
   writeUint32(header+34, 0);  //size of pixels, can use 0 when no compression
   writeUint32(header+38, 0);  //pixels per meter, horizontal
@@ -44,18 +43,15 @@ void writeBMP(string filename, unsigned char** pixels, uint32_t w, uint32_t h) {
   writeUint32(header+50, 0);  //number of important colors, 0 for all
   outfile.write((const char*)header, 54);
   delete[] header;
-  char zero = 0;
+  char zero[] = {0, 0, 0, 0};
   for(int row=0; row<h; row++) {
     int col;
     for(col=0; col<w; col++) {
-      outfile.write((const char*)&pixels[col][row+2], 1);
-      outfile.write((const char*)&pixels[col][row+1], 1);
-      outfile.write((const char*)&pixels[col][row+0], 1);
+      outfile.write((const char*)&pixels[col][row*3+2], 1);
+      outfile.write((const char*)&pixels[col][row*3+1], 1);
+      outfile.write((const char*)&pixels[col][row*3+0], 1);
     }
-    while(col%4 != 0) {
-      outfile.write(&zero, 1);
-      col++;
-    }
+    outfile.write(zero, (col*3)%4);
   }
   
   outfile.close();
--- src/tests.cpp
+++ src/tests.cpp
@@ -2,15 +2,20 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-  unsigned char **pixels = new unsigned char*[50];
-  for(int i=0; i<50; i++) {
+  unsigned char **pixels = new unsigned char*[150];
+  for(int i=0; i<150; i++) {
     pixels[i] = new unsigned char[50*3];
     for(int j=0; j<50; j++) {
-      pixels[i][j*3+0] = 255;
       pixels[i][j*3+1] = 0;
-      pixels[i][j*3+2] = 0;
+      if(i ==j) {
+        pixels[i][j*3+0] = 0;
+        pixels[i][j*3+2] = 255;
+      } else {
+        pixels[i][j*3+2] = 0;
+        pixels[i][j*3+0] = 255;
+      }
     }
   }
-  writeBMP("out.bmp", pixels, 50, 50);
+  writeBMP("out.bmp", pixels, 150, 50);
   std::cout << "Done" << std::endl;
 }
